5026dc1593ca69846897b9a37b193474c593cfe3
Myriad-86x <myriad.86x@gmail.com>
2020-03-10 14:42:18 +0200

    changes in footer

---
+++ scripts/footer.js
@@ -0,0 +1,7 @@
+document.getElementById("pageFooter").innerHTML = "<div>"
+    + "<a href='http://github.com/Myriad-86x' target='_blank'>"
+    + "<img src='icons/github-logo.svg' alt='github-logo'>"
+    + "</a>"
+    + "<a href='http://twitter.com' target='_blank'>"
+    + "<img src='icons/twitter.logo.svg' alt'twitter-logo'>"
+    + "</a>";
