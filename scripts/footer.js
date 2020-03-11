33fb0641dc32a8af0b5ec8e85e8552ec362ff7e5
Myriad-86x <myriad.86x@gmail.com>
2020-03-11 21:53:53 +0200

    some fixes in footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,8 @@
 document.getElementById("pageFooter").innerHTML = "<div>"
     + "<a href='http://github.com/Myriad-86x' target='_blank'>"
-    + "<img src='icons/github-logo.svg' alt='github-logo'>"
+    + "<img src='icons/github-logo.png' class='social-logo' alt='github-logo'>"
     + "</a>"
     + "<a href='http://twitter.com' target='_blank'>"
-    + "<img src='icons/twitter.logo.svg' alt'twitter-logo'>"
-    + "</a>";
+    + "<img src='icons/twitter-logo.png' class='social-logo' alt'twitter-logo'>"
+    + "</a>"
+    + "<p style='font-size: 10px'>© 2020 Myriad-86x</p>";
